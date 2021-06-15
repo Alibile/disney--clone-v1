17483d0b84df42c4eb76201a57d181d4dd6ebf9d
Alibile <cali.isxaakh@gmai.com>
2021-06-15 06:18:00 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
+import Headers from "./Components/Headers";
+import Login from "./Components/Login";
+import HomePage from "./Components/HomePage";
+import Detail from "./Components/Detail";
 function App() {
   return (
     <div className="App">
-      <h1> Disney clone</h1>
+      <Router>
+        <Headers />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
